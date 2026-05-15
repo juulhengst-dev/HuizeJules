a733e67443f5b5334122536cb236a6308a228867
juulhengst-dev <juulhengst@gmail.com>
2026-05-15 12:06:16 +0200

    Add files via upload

---
+++ vragen.js
@@ -0,0 +1,21 @@
+// Hier kun je zelf vragen toevoegen, aanpassen of verwijderen.
+// Elke vraag staat tussen aanhalingstekens en wordt afgesloten met een komma.
+// Minimaal evenveel vragen als het aantal rondes dat je instelt!
+
+const QUESTIONS = [
+  "Als dieren konden stemmen, welk dier zou altijd verliezen en waarom?",
+  "Wat zou de slechste superkracht zijn om te hebben tijdens een date?",
+  "Beschrijf de wereld als een koelkast die beslist heeft het op te geven.",
+  "Wat zou de slechtste naam zijn voor een kinderdagverblijf?",
+  "Als je leven een Netflix-serie was, wat zou de beschamende ondertitel zijn?",
+  "Wat is het domste dat iemand ooit met wetenschap kon bewijzen?",
+  "Beschrijf maandag als een persoon op een feestje.",
+  "Wat zou de meest nutteloze uitvinding ooit zijn?",
+  "Als emoties belasting betaalden, welke zou het meeste schulden hebben?",
+  "Wat zou een hond schrijven in zijn dagboek na een slechte dag?",
+  "Bedenk een waarschuwingslabel voor mensen.",
+  "Wat zou jouw superheldennaam zijn als je superpower 'slecht parkeren' was?",
+  "Als je een museum mocht openen over jouw leven, wat zou de eerste zaal heten?",
+  "Bedenk een slogan voor de dag van morgen.",
+  "Wat zou de slechtste bijbaan zijn voor een spook?",
+];
